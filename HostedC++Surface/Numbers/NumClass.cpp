5ea47d020f90ef5e8280ee8b7e235cf3a5a0755d
toddland <tlandstad@hotmail.com>
2016-11-14 21:41:13 -0800

    Init version

---
+++ HostedC++Surface/Numbers/NumClass.cpp
@@ -0,0 +1,54 @@
+﻿#include "NumClass.h"
+#include <stdlib.h>   
+#include <cinttypes>
+
+using namespace Numbers;
+using namespace Platform;
+using u8_8 = std::uint16_t;
+
+int num;
+
+NumClass::NumClass() {
+	num = 0;
+}
+
+int NumClass::randomNumber(int min, int max) {
+	int diff = max - min + 1;
+	return rand() % diff + min;
+}
+
+int NumClass::getNum() {
+	return num;
+}
+
+void NumClass::setNum(int x) {
+	num = x;
+}
+
+int NumClass::factorial(int x)
+{
+	if (x == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return x*factorial(x - 1);
+	}
+}
+
+float NumClass::add(float a, float b) {
+	return fixed_to_float(addHelper(float_to_fixed(a), float_to_fixed(b)));
+}
+
+u8_8 NumClass::float_to_fixed(float f) {
+	return f * 256;
+}
+
+float NumClass::fixed_to_float(u8_8 i) {
+	return i / 256.f;
+}
+
+u8_8 NumClass::addHelper(u8_8 a, u8_8 b) {
+	return a + b;
+}
---
+++ HostedC++Surface/Numbers/NumClass.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <cinttypes>
+using u8_8 = std::uint16_t;
+
+namespace Numbers
+{
+	public ref class NumClass sealed
+	{
+	public:
+		NumClass();
+		int randomNumber(int min, int max);
+		void setNum(int x);
+		int getNum();
+		int factorial(int x);
+		float add(float a, float b);
+		u8_8 float_to_fixed(float f);
+		float fixed_to_float(u8_8 i);
+		u8_8 addHelper(u8_8 a, u8_8 b);
+	};
+}
+
